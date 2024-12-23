5193eb81a8a47eafebdabf5868f69e799c017080
nefic24 <hnefic@gmail.com>
2024-12-23 21:28:11 +0100

    Dodavanje treceg atributa

--- vjezba1.py
+++ vjezba1.py
@@ -1,4 +1,13 @@
 # Ovo je template za klasu
+
+"""
+Initialize the class with atribute1 and atribute2.
+
+args:
+atribute1 (type)
+atribute2 (type)
+"""
+
 class Vozilo:
     def __init__(self, marka):
         self.marka = marka
