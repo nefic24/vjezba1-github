0f38ebfd082197fc44e464d475be8eff56776fcc
nefic24 <hnefic@gmail.com>
2024-12-23 20:18:31 +0100

    dodajemo naš prvi fajl na github

---
+++ vjezba1.py
@@ -0,0 +1,11 @@
+class Vozilo:
+    def __init__(self, marka):
+        self.marka = marka
+
+    def info(self):
+        print(f"Marka: {self.marka}")
+
+class Auto(Vozilo):
+    def __init__(self, marka, model):
+        super().__init__(marka)
+        self.model = model
