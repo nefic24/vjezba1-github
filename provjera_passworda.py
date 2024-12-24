7bbad7b14902b323413513afd1ec65786a84b093
nefic24 <hnefic@gmail.com>
2024-12-24 17:27:16 +0100

    Komentar za fajl

--- provjera_passworda.py
+++ provjera_passworda.py
@@ -1,3 +1,11 @@
+"""
+Initialize the class with atribute1 and atribute2.
+
+args:
+atribute1 (type)
+atribute2 (type)
+"""
+
 provjer_passw.py
 def provjera_passworda(password):
     if len(password):
